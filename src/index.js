56bd5f491362f5ca272157f1286cb833bba3a059
yavb.code <yavb.code@gmail.com>
2021-04-03 15:43:35 -0500

    add configurarion extra

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
 //CONFIGURANDO EL SERVIDOR
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
 
 //IMPORTANDO RUTAS
 app.use(require("./routes/index"));
